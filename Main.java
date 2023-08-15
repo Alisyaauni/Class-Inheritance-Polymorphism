2589e8c0e5158c14f23eea6ee3221140bd5c6a14
Alisya Auni <139848238+Alisyaauni@users.noreply.github.com>
2023-08-15 16:25:02 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Phone f = new Phone();              //creating object for phone
+        Iphone i = new Iphone();            //creating object for iphone
+
+        System.out.println("Call Phone");
+        i.call();                           //display call sound of an iphone
+
+    }
+}
---
+++ Phone.java
@@ -0,0 +1,13 @@
+public class Phone {
+    public void call(){                       //call phone method
+        System.out.println("Ringgg RInggg");
+    }
+}
+
+class Iphone extends Phone {
+        public void call(){                   //call phone method for Iphone(same name method)
+            System.out.println("tuuut tuuuut");
+
+        }
+}
+
